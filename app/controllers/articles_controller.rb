3870d5ec20bdb06b8c2eaad62bdffe7b7bf9b23d
carl-altvr <carl@altvr.com>
2016-03-03 18:56:35 -0800

    8.2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,10 @@
 class ArticlesController < ApplicationController
- def index
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+  def index
     @articles = Article.all
- end
+  end
 
   def show
     @article = Article.find(params[:id])
